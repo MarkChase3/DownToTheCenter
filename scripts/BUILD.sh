598ca67d1f781fb9bc4ad56a382577047fe066f4
MarkChase3 <bittencourt2591@gmail.com>
2023-01-25 15:35:11 -0300

    Update stuff

--- scripts/BUILD.sh
+++ scripts/BUILD.sh
@@ -1,6 +1,13 @@
 cd code
 go build
 GOOS=windows go build
+GOOS=darwin GOARCH=go build
+STR1=$(base64 -w 0 ../images/player.png)
+STR2=$(base64 -w 0 ../images/tileset1.png)
+STR3=$(base64 -w 0 ../maps/jsons/map1.json)
+sed -i "s@IMAGES_PLAYER_DATA@$STR1@g" fs/fs_wasm.go
+sed -i "s@IMAGES_TILESET1_DATA@$STR2@g" fs/fs_wasm.go
+sed -i "s@MAPS_JSONS_MAP1_DATA@$STR3@g" fs/fs_wasm.go
 GOOS=js GOARCH=wasm go build -o DownToTheCenter.wasm
 mv DownToTheCenter ../build/DownToTheCenter
 mv DownToTheCenter.exe ../build/DownToTheCenter.exe
